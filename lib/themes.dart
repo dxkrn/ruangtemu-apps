e928c71df7360d80a2f573a00ffdf8018f65f9cf
dxkrnn <dxkrn@gmail.com>
2022-09-15 22:50:30 +0700

    Change fontSize value on themes

--- lib/themes.dart
+++ lib/themes.dart
@@ -9,25 +9,25 @@ Color greyColor = const Color.fromARGB(255, 208, 205, 227);
 
 TextStyle heading1BoldTextStyle = GoogleFonts.poppins(
   fontWeight: FontWeight.bold,
-  fontSize: 15.sp,
+  fontSize: 16.sp,
 );
 
 TextStyle heading1MediumTextStyle = GoogleFonts.poppins(
   fontWeight: FontWeight.w500,
-  fontSize: 15.sp,
+  fontSize: 16.sp,
 );
 
 TextStyle heading2TextStyle = GoogleFonts.poppins(
   fontWeight: FontWeight.w500,
-  fontSize: 12.sp,
+  fontSize: 14.sp,
 );
 
 TextStyle heading3TextStyle = GoogleFonts.poppins(
   fontWeight: FontWeight.w500,
-  fontSize: 10.sp,
+  fontSize: 12.sp,
 );
 
 TextStyle heading4TextStyle = GoogleFonts.poppins(
   fontWeight: FontWeight.w500,
-  fontSize: 8.sp,
+  fontSize: 10.sp,
 );
