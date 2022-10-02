dfc3fa8a0a9a403e523e18c52ecab2f5fc74bb00
dxkrnn <dxkrn@gmail.com>
2022-10-02 20:18:27 +0700

    Fetch data from api on Ruang Survey [http, listview]

---
+++ lib/Models/survey.dart
@@ -0,0 +1,63 @@
+// To parse this JSON data, do
+//
+//     final callcenter = callcenterFromJson(jsonString);
+
+import 'dart:convert';
+
+List<Survey> callcenterFromJson(String str) =>
+    List<Survey>.from(json.decode(str).map((x) => Survey.fromJson(x)));
+
+String callcenterToJson(List<Survey> data) =>
+    json.encode(List<dynamic>.from(data.map((x) => x.toJson())));
+
+class Survey {
+  Survey({
+    required this.id,
+    required this.name,
+    required this.description,
+    required this.url,
+    required this.logoUrl,
+    required this.authorID,
+    required this.clickCount,
+    required this.createdAt,
+    required this.updatedAt,
+    required this.author,
+  });
+
+  int id;
+  String name;
+  String description;
+  String url;
+  dynamic logoUrl;
+  int authorID;
+  int clickCount;
+  DateTime createdAt;
+  DateTime updatedAt;
+  Map author;
+
+  factory Survey.fromJson(Map<String, dynamic> json) => Survey(
+        id: json["id"],
+        name: json["name"],
+        description: json["description"],
+        url: json["url"],
+        logoUrl: json["logo_url"],
+        authorID: json["author_id"],
+        clickCount: json["click_count"],
+        createdAt: DateTime.parse(json["created_at"]),
+        updatedAt: DateTime.parse(json["updated_at"]),
+        author: json["author"],
+      );
+
+  Map<String, dynamic> toJson() => {
+        "id": id,
+        "name": name,
+        "description": description,
+        "url": url,
+        "logo_url": logoUrl,
+        "author_id": authorID,
+        "click_count": clickCount,
+        "created_at": createdAt.toIso8601String(),
+        "updated_at": updatedAt.toIso8601String(),
+        "author": author,
+      };
+}
--- lib/Pages/Features/Survey/ruang_survey.dart
+++ lib/Pages/Features/Survey/ruang_survey.dart
@@ -1,18 +1,50 @@
 // ignore_for_file: must_be_immutable
 
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:ruang_temu_apps/Models/survey.dart';
 import 'package:ruang_temu_apps/Widgets/feature_appbar.dart';
+import 'package:http/http.dart' as http;
+import 'package:url_launcher/url_launcher.dart';
 
+import '../../../env.dart';
 import '../../../themes.dart';
 
-class RuangSurvey extends StatelessWidget {
+Future<List<Survey>> fetchSurvey() async {
+  final response = await http.get(Uri.parse("$baseAPIUrl/surveys"));
+
+  if (response.statusCode == 200) {
+    Iterable l = json.decode(response.body);
+    return List<Survey>.from(l.map((model) => Survey.fromJson(model)));
+  } else {
+    // If the server did not return a 200 OK response,
+    // then throw an exception.
+    throw Exception('Failed to load survey');
+  }
+}
+
+class RuangSurvey extends StatefulWidget {
   const RuangSurvey({super.key});
 
   @override
+  State<RuangSurvey> createState() => _RuangSurveyState();
+}
+
+class _RuangSurveyState extends State<RuangSurvey> {
+  late Future<List<Survey>> futureSurvey;
+
+  @override
+  void initState() {
+    super.initState();
+    futureSurvey = fetchSurvey();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    // double deviceWidth = MediaQuery.of(context).size.width;
-    double deviceHeight = MediaQuery.of(context).size.height;
+    double deviceWidth = MediaQuery.of(context).size.width;
+    // double deviceHeight = MediaQuery.of(context).size.height;
     return Scaffold(
       appBar: FeatureAppbar(
         title: 'Ruang Survey',
@@ -33,17 +65,23 @@ class RuangSurvey extends StatelessWidget {
           ),
           ListView(
             children: [
-              Container(
+              SizedBox(
+                // height: deviceHeight - 170.h,
                 height: 200.h,
               ),
               Container(
-                height: deviceHeight - 200.h,
+                height: 70.h,
+                // height: (listLength < 4) ? deviceHeight - 200.h : 100.h,
                 decoration: BoxDecoration(
-                  borderRadius: const BorderRadius.only(
-                    topLeft: Radius.circular(30),
-                    topRight: Radius.circular(30),
-                  ),
                   color: blueColor,
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(30.r),
+                    topRight: Radius.circular(30.r),
+                  ),
+                  border: Border.all(
+                    width: 0,
+                    color: blueColor,
+                  ),
                 ),
                 child: Column(
                   children: [
@@ -51,45 +89,85 @@ class RuangSurvey extends StatelessWidget {
                       height: 10.h,
                     ),
                     Container(
-                      width: 50.w,
-                      height: 2.h,
+                      width: 70.w,
+                      height: 3.h,
                       decoration: BoxDecoration(
                         color: whiteColor,
                         borderRadius: BorderRadius.circular(30.r),
                       ),
                     ),
-                    SizedBox(
-                      height: 20.h,
-                    ),
                     Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(30.r),
+                        color: blueColor,
+                      ),
                       padding: EdgeInsets.symmetric(
-                        horizontal: 20.w,
+                        horizontal: 10.w,
+                        vertical: 10.h,
                       ),
                       child: Text(
-                        'Ruang Survey berisi survey menarik yang dikelola oleh Kementerian Riset dan Data BEM KM UNY',
+                        'Ruang Survey berisi survey menarik yang dikelola oleh Kementerian Riset dan Data BEM KM UNY ',
                         style: heading3TextStyle.copyWith(
                           color: whiteColor,
                         ),
                         textAlign: TextAlign.center,
                       ),
                     ),
-                    SizedBox(
-                      height: 20.h,
-                    ),
-                    SurveyCard(
-                      id: '1',
-                      title:
-                          'Survey Kepuasan Mahasiswa Terhadap  Fasilitas Kampus',
-                    ),
-                    SurveyCard(
-                      id: '2',
-                      title: 'Survey Pembelajaran Offline',
-                    ),
-                    SurveyCard(
-                      id: '3',
-                      title: 'Survey Kepuasan Ruang Temu Apps',
-                    ),
                   ],
+                ),
+              ),
+
+              //card builder
+              FutureBuilder<List<Survey>>(
+                future: futureSurvey,
+                builder: (context, snapshot) {
+                  if (snapshot.hasData) {
+                    return Column(
+                        children: snapshot.data
+                                ?.map(
+                                  (e) => SurveyCard(
+                                    title: e.name,
+                                    url: e.url,
+                                  ),
+                                )
+                                .toList() ??
+                            []);
+                  } else if (!snapshot.hasData) {
+                    return Container(
+                      width: deviceWidth,
+                      height: 300.h,
+                      decoration: BoxDecoration(
+                        color: blueColor,
+                        border: Border.all(
+                          width: 0,
+                          color: blueColor,
+                        ),
+                      ),
+                      child: const Center(
+                        child: CircularProgressIndicator(
+                          color: Colors.white,
+                        ),
+                      ),
+                    );
+                  } else if (snapshot.hasError) {
+                    return Text('${snapshot.error}');
+                  }
+
+                  // By default, show a loading spinner.
+                  return const CircularProgressIndicator(
+                    color: Colors.white,
+                  );
+                },
+              ),
+              Container(
+                width: deviceWidth,
+                height: 100.h,
+                decoration: BoxDecoration(
+                  color: blueColor,
+                  border: Border.all(
+                    width: 0,
+                    color: blueColor,
+                  ),
                 ),
               ),
             ],
@@ -100,61 +178,80 @@ class RuangSurvey extends StatelessWidget {
   }
 }
 
-class SurveyCard extends StatelessWidget {
-  SurveyCard({
-    Key? key,
-    required this.id,
-    required this.title,
-  }) : super(key: key);
+class SurveyCard extends StatefulWidget {
+  SurveyCard({Key? key, required this.title, required this.url})
+      : super(key: key);
 
-  String id;
   String title;
+  String url;
+
+  @override
+  State<SurveyCard> createState() => _SurveyCardState();
+}
+
+class _SurveyCardState extends State<SurveyCard> {
+  //Function for Launch URL
+  openUrl() async {
+    await launchUrl(Uri.parse(widget.url));
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        GestureDetector(
-          onTap: () {},
-          child: Container(
-            padding: EdgeInsets.all(10.w),
-            width: 320.w,
-            height: 75.h,
-            decoration: BoxDecoration(
-              color: whiteColor,
-              borderRadius: BorderRadius.circular(20.r),
-            ),
-            child: Row(
-              children: [
-                SizedBox(
-                  width: 40.h,
-                  height: 40.h,
-                  child: const Image(
-                    image: AssetImage('assets/icons/icon_survey_light.png'),
-                  ),
-                ),
-                SizedBox(
-                  width: 10.w,
-                ),
-                Container(
-                  alignment: Alignment.centerLeft,
-                  width: 240.w,
-                  height: 55.h,
-                  child: Text(
-                    title,
-                    style: heading2TextStyle.copyWith(
-                      color: blueColor,
+    double deviceWidth = MediaQuery.of(context).size.width;
+    return Container(
+      width: deviceWidth,
+      decoration: BoxDecoration(
+          color: blueColor,
+          border: Border.all(
+            width: 0,
+            color: blueColor,
+          )),
+      child: Column(
+        children: [
+          GestureDetector(
+            onTap: () {
+              openUrl();
+            },
+            child: Container(
+              padding: EdgeInsets.all(10.w),
+              width: 320.w,
+              height: 75.h,
+              decoration: BoxDecoration(
+                color: whiteColor,
+                borderRadius: BorderRadius.circular(20.r),
+              ),
+              child: Row(
+                children: [
+                  SizedBox(
+                    width: 40.h,
+                    height: 40.h,
+                    child: const Image(
+                      image: AssetImage('assets/icons/icon_survey_light.png'),
                     ),
                   ),
-                ),
-              ],
+                  SizedBox(
+                    width: 10.w,
+                  ),
+                  Container(
+                    alignment: Alignment.centerLeft,
+                    width: 240.w,
+                    height: 55.h,
+                    child: Text(
+                      widget.title,
+                      style: heading2TextStyle.copyWith(
+                        color: blueColor,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
-        ),
-        SizedBox(
-          height: 20.h,
-        ),
-      ],
+          SizedBox(
+            height: 20.h,
+          ),
+        ],
+      ),
     );
   }
 }
